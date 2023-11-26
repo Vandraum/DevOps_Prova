ea6c3348e948e7e92b72f50934c7f80199cc8d2e
Vandraum <99769306+Vandraum@users.noreply.github.com>
2023-11-26 19:31:27 -0300

    puxando o index.hmtl

--- app.py
+++ app.py
@@ -1,20 +1,6 @@
-from flask import Flask, render_template
-import os
-
+from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    current_directory = os.path.dirname(os.path.abspath(__file__))
- 
-    index_path = os.path.join(current_directory, 'index.html')
-    
-    if os.path.exists(index_path):
-        with open(index_path, 'r') as file:
-            content = file.read()
-        return content
-    else:
-        return "Erro: arquivo 'index.html' não encontrado."
-
-if __name__ == '__main__':
-    app.run()
+def Home_Page():
+  return "index.html"
