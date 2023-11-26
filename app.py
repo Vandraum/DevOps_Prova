e3d72219e0acfb4fb79980d9bf8682d1bcfe18a9
Vandraum <99769306+Vandraum@users.noreply.github.com>
2023-11-26 19:24:45 -0300

    Puxar a Pagina HTML

--- app.py
+++ app.py
@@ -1,6 +1,20 @@
-from flask import Flask
+from flask import Flask, render_template
+import os
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-  return "Hello, World!"
+    current_directory = os.path.dirname(os.path.abspath(__file__))
+ 
+    index_path = os.path.join(current_directory, 'index.html')
+    
+    if os.path.exists(index_path):
+        with open(index_path, 'r') as file:
+            content = file.read()
+        return content
+    else:
+        return "Erro: arquivo 'index.html' não encontrado."
+
+if __name__ == '__main__':
+    app.run()
