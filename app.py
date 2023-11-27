4c4f78b724b02e5f9b537e62a71d49185d635126
Vandraum <99769306+Vandraum@users.noreply.github.com>
2023-11-26 21:42:55 -0300

    Update app.py

--- app.py
+++ app.py
@@ -4,21 +4,17 @@ import os
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    try:
-       
-        current_directory = os.path.dirname(os.path.abspath(__file__))
+def Home_Page():
+    current_directory = os.path.dirname(os.path.abspath(__file__))
 
-        
-        index_path = os.path.join(current_directory, 'templates', 'index.html')
+    index_path = os.path.join(current_directory, 'index.html')
 
-        
-        if os.path.exists(index_path):
-            return render_template('index.html')
-        else:
-            return "Erro: arquivo 'index.html' não encontrado."
-    except Exception as e:
-        return f"Erro interno: {str(e)}"
+    if os.path.exists(index_path):
+        with open(index_path, 'r') as file:
+            content = file.read()
+        return content
+    else:
+        return "Erro: arquivo 'index.html' não encontrado."
 
 if __name__ == '__main__':
     app.run()
