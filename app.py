bb4420edf2692594a74a65db3477c80c9a2eaec7
Vandraum <99769306+Vandraum@users.noreply.github.com>
2023-11-29 20:46:26 -0300

    TESTE app.py

--- app.py
+++ app.py
@@ -1,20 +1,5 @@
-from flask import Flask, render_template
-import os
-
+from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def Home_Page():
-    current_directory = os.path.dirname(os.path.abspath(__file__))
-
-    index_path = os.path.join(current_directory, 'index.html')
-
-    if os.path.exists(index_path):
-        with open(index_path, 'r') as file:
-            content = file.read()
-        return content
-    else:
-        return "Erro: arquivo 'index.html' não encontrado."
-
-if __name__ == '__main__':
-    app.run()
+def hello_world():
